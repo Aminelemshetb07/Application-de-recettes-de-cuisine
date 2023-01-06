7e14c681048b12a7e36b36d58b7c6e4eef02619a
Aminelemshetb07 <alamchatab@gmail.com>
2023-01-06 15:24:40 +0000

    Add categories && countries

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // take data in randomData
 let randomData = [];
 for (let i = 0; i < 6; i++) {
-    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then((response) => response.json())
     .then((data) => randomData.push(data.meals[0]));
 }
@@ -20,16 +20,21 @@ for (let i = 0; i < 6; i++) {
 // }
 
 setTimeout(() => {
-    buildeCard(randomData);
-}, 500);
-// function build 
+  buildeCard(randomData);
+}, 1000);
+// function build
 function buildeCard(array) {
-    console.log(array);
-    let CardsContainer = document.querySelector("#CardsContainer");
-    CardsContainer.innerHTML = "";
+  console.log(array);
+  let CardsContainer = document.querySelector("#CardsContainer");
+  CardsContainer.innerHTML = "";
+  let row;
 
-    let row;
-    for (let i = 0; i < array.length; i++) {
+  // if(array.length === null){
+  //     console.log("hahahah");
+  //     return;
+  // }
+
+  for (let i = 0; i < array.length; i++) {
     row = `<div class=" card m-3"  style="width: 20rem;">
                         <img src="${array[i].strMealThumb}" class="card-img-top" alt="CuisinMarccaine">
                         <div class="card-body">
@@ -39,53 +44,59 @@ function buildeCard(array) {
                         </div>
                     </div> `;
     CardsContainer.innerHTML += row;
-    }
+  }
 }
 // function for modal
 async function showModal(id) {
-    const response = await fetch(
+  const response = await fetch(
     "https://themealdb.com/api/json/v1/1/lookup.php?i=" + id
-    );
-    const data = await response.json();
-    console.log(data);
-    console.log(id);
-    let mealInfo = document.querySelector("#mealInfo");
-    let Ingredients;
-    let Measures;
-    Ingredients = "";
-    Measures = "";
-    // for loop for Ingredient && Measure
-    for (i = 1; i <= 20; i++) {
-        if (
-            data.meals[0]["strIngredient" + i] !== null &&
-            data.meals[0]["strIngredient" + i].length > 0 &&
-            data.meals[0]["strIngredient" + i] != " "
-        ) {
-        Ingredients += ` <li> ${data.meals[0]["strIngredient" + i]}</li>`;
-        }
-        if (
-            data.meals[0]["strMeasure" + i] !== null &&
-            data.meals[0]["strMeasure" + i].length > 0 &&
-        data.meals[0]["strMeasure" + i] != " "
-        ) {
-        Measures += ` <li> ${data.meals[0]["strMeasure" + i]} </li> `;
-        }
+  );
+
+  const data = await response.json();
+  console.log(data);
+  console.log(id);
+  let mealInfo = document.querySelector("#mealInfo");
+  let Ingredients;
+  let Measures;
+  Ingredients = "";
+  Measures = "";
+  // for loop for Ingredient && Measure
+  for (i = 1; i <= 20; i++) {
+    if (
+      data.meals[0]["strIngredient" + i] !== null &&
+      data.meals[0]["strIngredient" + i].length > 0 &&
+      data.meals[0]["strIngredient" + i] != " "
+    ) {
+      Ingredients += ` <li> ${data.meals[0]["strIngredient" + i]}</li>`;
     }
-    mealInfo.innerHTML = `
+    if (
+      data.meals[0]["strMeasure" + i] !== null &&
+      data.meals[0]["strMeasure" + i].length > 0 &&
+      data.meals[0]["strMeasure" + i] != " "
+    ) {
+      Measures += ` <li> ${data.meals[0]["strMeasure" + i]} </li> `;
+    }
+  }
+  mealInfo.innerHTML = `
     <div class="d-flex ">
         <div>
             <img src="${data.meals[0].strMealThumb}"
             class="card-img-top imgDetail img-fluid" alt="CuisinMarccaine">
             <iframe width="320" height="200" class="mt-2" src="${data.meals[0].strYoutube.replace(
-                "https://www.youtube.com/watch?v=",
-                "https://www.youtube.com/embed/" )}
+              "https://www.youtube.com/watch?v=",
+              "https://www.youtube.com/embed/"
+            )}
             "title="Tips For Using Async/Await in JavaScript" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div class="border ms-5 w-100">
             <p class="card-title p-2">Nom: ${data.meals[0].strMeal}</p>
-            <p class="card-title p-2">Categorie: ${data.meals[0].strCategory}</p>
+            <p class="card-title p-2">Categorie: ${
+              data.meals[0].strCategory
+            }</p>
             <p class="card-title p-2">Region: ${data.meals[0].strArea}</p>
-            <p class="card-title p-2 border">${data.meals[0].strInstructions}</p>
+            <p class="card-title p-2 border">${
+              data.meals[0].strInstructions
+            }</p>
             <div class="border w-100 d-flex">
                 <div>
                     <h5>Ingredients</h5>
@@ -97,20 +108,118 @@ async function showModal(id) {
             </div>
         </div>
     </div>`;
-    console.log(Ingredients);
+  console.log(Ingredients);
 }
-// function onkeyup search 
-$("#Search").on("click", function () {
-    let value = $(this).val();
-    console.log("value:", value);
-    async function search(value) {
-    const res = await fetch("https://themealdb.com/api/json/v1/1/search.php?s=" + value);
+// function onkeyup search
+$("#SearchInput").on("keyup", function () {
+  let value = $(this).val();
+  console.log("value:", value);
+  async function search(value) {
+    const res = await fetch(
+      "https://themealdb.com/api/json/v1/1/search.php?s=" + value
+    );
     const dataSearch = await res.json();
     console.log(dataSearch.meals);
     buildeCard(dataSearch.meals);
-    }
-search(value);
+  }
+  search(value);
 });
+
+// add a variable to storing data
+let Categories = [];
+let Countries = [];
+// add a categories
+async function getCartegories() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+  Categories = await response.json();
+  console.log(Categories);
+  addCategories(Categories.categories);
+}
+getCartegories();
+// add categorie in html
+function addCategories(data) {
+  for (let i = 0; i < data.length; i++) {
+    let meals = `<option value="${data[i].strCategory}">${data[i].strCategory}</option>`;
+    document.querySelector("#selectCategory").innerHTML += meals;
+  }
+  console.log(data);
+}
+// function to get countries
+async function getCountries() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+  );
+  Countries = await response.json();
+  console.log(Countries);
+  addCountries(Countries.meals);
+}
+getCountries();
+// add countries in html
+function addCountries(data) {
+    for (let i = 0; i < data.length; i++) {
+    let meals = `<option value="${data[i].strArea}">${data[i].strArea}</option>`;
+    document.querySelector("#selectCountry").innerHTML += meals;
+    }
+    console.log(data);
+}
+
+document.getElementById("search").onclick = async function () {
+    let selectedCategory = document.getElementById("selectCategory").value;
+    let selectedCountry = document.getElementById("selectCountry").value;
+    let dataCountry, dataCategory;
+let contryAndCategoryId ;
+    
+  // console.log(selectedCategory, selectedCountry)
+
+    const response = await fetch
+    (
+    "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory
+    );
+    dataCategory = await response.json();
+  // console.log(dataCategory);
+
+    const res = await fetch
+    (
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + selectedCountry
+    );
+    dataCountry = await res.json();
+  // console.log(dataCountry);
+    console.log(dataCountry, dataCategory);
+
+    for (let i = 0; i < dataCategory.length; i++) {
+        for (let j = 0; j < dataCountry.length; j++) {
+            if (dataCategory[i] === dataCountry[j])
+            contryAndCategoryId.push(dataCategory[i]);
+        }
+    }
+
+console.log(contryAndCategoryId)
+
+
+};
+
+
+
+
+
+
+
+
+
+// function BuildCardById(array1, array2) {
+//     let result = [];
+//       for (let i = 0; i < array1.length; i++) {
+//         for (let j = 0; j < array2.length; j++) {
+//           if (array1[i].idMeal === array2[j].idMeal) {
+//             result.push(array1[i]);
+//           }
+//               }
+//       }
+//     BuildCard(result)
+//   }
+
 
 
 
